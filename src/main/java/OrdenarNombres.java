98a019dbd6849eb58fcb4fea218c4d51b66a34f6
vitaliy-pg <aljndrvt@myuax.com>
2024-04-23 18:08:54 +0200

    nombre de las personas se pueden cambiar pero que pereza

--- src/main/java/OrdenarNombres.java
+++ src/main/java/OrdenarNombres.java
@@ -3,4 +3,12 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class OrdenarNombres {
-}
+
+    public static void main(String[] args) {
+        Set<String> nombres = new TreeSet<>();
+        nombres.add("Ana");
+        nombres.add("Pedro");
+        nombres.add("Maria");
+        nombres.add("Carlos");
+
+    }
