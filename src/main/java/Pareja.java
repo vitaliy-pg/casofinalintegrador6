3e3d2bd5e8e95c698b255a5fa0b3141552179c80
vitaliy-pg <aljndrvt@myuax.com>
2024-04-23 18:15:29 +0200

    retoma valores y claves

--- src/main/java/Pareja.java
+++ src/main/java/Pareja.java
@@ -1,4 +1,27 @@
 
 
-public class Pareja {
+public class Pareja<T, U> {
+    private T clave;
+    private U valor;
+
+    public Pareja(T clave, U valor) {
+        this.clave = clave;
+        this.valor = valor;
+    }
+
+    public T getClave() {
+        return clave;
+    }
+
+    public U getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Clave: " + clave + ", Valor: " + valor;
+    }
+
+
+
 }
