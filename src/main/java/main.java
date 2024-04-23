1b1f4ec768b2de6e0194a399168f33fe46951a77
vitaliy-pg <aljndrvt@myuax.com>
2024-04-23 18:22:40 +0200

    parte de gestion de datos dinamicos

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,6 +3,14 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class main {
-
+public class main extends JFrame {
+    public GestionDatosDinamicos() {
+        setTitle("Gestión de Datos Dinámicos");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        initializeComponents();
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
