aeed59d44d5994253b3f9b46ac5ea976fc2ec1e6
vitaliy-pg <aljndrvt@myuax.com>
2024-04-23 18:01:08 +0200

    letras asociadas a numeros

--- src/main/java/AsociacionNumerosLetras.java
+++ src/main/java/AsociacionNumerosLetras.java
@@ -1,4 +1,11 @@
 import java.util.HashMap;
 import java.util.Map;
 public class AsociacionNumerosLetras {
-}
+    public static void main(String[] args) {
+        Map<Integer, Character> numerosLetras = new HashMap<>();
+        numerosLetras.put(1, 'A');
+        numerosLetras.put(2, 'B');
+        numerosLetras.put(3, 'C');
+        numerosLetras.put(4, 'D');
+
+    }
