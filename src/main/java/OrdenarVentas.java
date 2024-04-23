6c9d00129ca51c155634ee6651e8aa664ca6effc
vitaliy-pg <aljndrvt@myuax.com>
2024-04-23 18:10:59 +0200

    clase para las ventas

--- src/main/java/OrdenarVentas.java
+++ src/main/java/OrdenarVentas.java
@@ -4,4 +4,12 @@ import java.util.Comparator;
 import java.util.List;
 
 public class OrdenarVentas {
+
+    String producto;
+    int cantidad;
+
+    public Ventas(String producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
 }
