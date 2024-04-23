4b04a6d8a1efccdf5fe55bdbf471ea41bbaf0f85
vitaliy-pg <aljndrvt@myuax.com>
2024-04-23 17:58:54 +0200

    ruta y nombre de los archivos

--- src/main/java/Archivos.java
+++ src/main/java/Archivos.java
@@ -1,2 +1,8 @@
-public class Archivos {
-}
+class Archivos implements Comparable<Archivos> {
+    final String nombre;
+    private final String ruta;
+
+    public Archivos(String nombre, String ruta) {
+        this.nombre = nombre;
+        this.ruta = ruta;
+    }
