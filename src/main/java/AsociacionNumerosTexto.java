cb43cad9aa83379213ff0d7eb6f266ab1d1f1b42
vitaliy-pg <aljndrvt@myuax.com>
2024-04-23 18:02:54 +0200

    nuemeros

--- src/main/java/AsociacionNumerosTexto.java
+++ src/main/java/AsociacionNumerosTexto.java
@@ -3,4 +3,10 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class AsociacionNumerosTexto {
+    public static void main(String[] args) {
+        Map<Integer, String> numerosTexto = new HashMap<>();
+        numerosTexto.put(1, "Uno");
+        numerosTexto.put(2, "Dos");
+        numerosTexto.put(3, "Tres");
+        numerosTexto.put(4, "Cuatro");
 }
