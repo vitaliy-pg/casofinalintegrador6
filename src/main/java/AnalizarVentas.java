0559e367f429b764732acb88d9511dcb1a569420
vitaliy-pg <aljndrvt@myuax.com>
2024-04-23 17:55:51 +0200

    objetos de ventas aunuqe se puden añadir mas

--- src/main/java/AnalizarVentas.java
+++ src/main/java/AnalizarVentas.java
@@ -3,4 +3,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class AnalizarVentas {
-}
+    public static void main(String[] args) {
+        List<Ventas> ventas = List.of(
+                new Ventas("Manzanas", 50),
+                new Ventas("Platanos", 20),
+                new Ventas("Naranjas", 70)
+        );
+
+    }
