be2d9cd91c8b073120500b8a00f38844dece2492
vitaliy-pg <aljndrvt@myuax.com>
2024-04-23 18:05:50 +0200

    crear directorios

--- src/main/java/IndexadorArchivos.java
+++ src/main/java/IndexadorArchivos.java
@@ -4,4 +4,19 @@ import java.util.Collections;
 import java.util.List   ;
 
 public class IndexadorArchivos {
+
+    private List<Archivos> archivos = new ArrayList<>();
+
+    public void indexarDirectorio(String rutaDirectorio) {
+        File directorio = new File(rutaDirectorio);
+        File[] archivosEnDirectorio = directorio.listFiles();
+        if (archivosEnDirectorio != null) {
+            for (File Archivos : archivosEnDirectorio) {
+                if (Archivos.isFile()) {
+                    archivos.add(new Archivos(Archivos.getName(), Archivos.getAbsolutePath()));
+                } else if (Archivos.isDirectory()) {
+                    indexarDirectorio(Archivos.getAbsolutePath());
+                }
+            }
+        }
 }
